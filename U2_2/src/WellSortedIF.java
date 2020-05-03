1888192272df6f7b10e456e3c5e04f723e48fc75
Felix Knorre <felix.knorre@smail.inf.h-brs.de>
2020-05-03 11:17:46 +0200

    add u2

---
+++ U2_2/src/WellSortedIF.java
@@ -0,0 +1,4 @@
+
+public interface WellSortedIF {
+	public boolean isWellSorted(String[] sequence);
+}
---
+++ U2_2/src/WellSortedIFTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WellSortedIFTest {
+
+	@Test
+	void testIsWellSorted() {
+		fail("Not yet implemented");
+	}
+
+}
