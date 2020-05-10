ed4950c8336989801f9436dc2f6bd7e12259fe0d
Felix Knorre <felix.knorre@smail.inf.h-brs.de>
2020-05-10 13:24:08 +0200

    added Parkhaus

--- Parkhaus/WebContent/index.jsp
+++ Parkhaus/WebContent/index.jsp
@@ -5,8 +5,12 @@
 <head>
 <meta charset="UTF-8">
 <title>Parkhaus</title>
+<script src='https://ccmjs.github.io/mkaul-components/parkhaus/versions/ccm.parkhaus-7.0.0.js'></script>
 </head>
 <body>
 	<h1>Parkhaus</h1>
+	<ccm-parkhaus-7-0-0
+		server_url="http://localhost:8080/Parkhaus/Parkhaus"
+	></ccm-parkhaus-7-0-0>
 </body>
 </html>
--- Parkhaus/src/DemoServlet.java
+++ Parkhaus/src/DemoServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class DemoServlet
  */
-@WebServlet("/DemoServlet")
+@WebServlet("/Parkhaus")
 public class DemoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -30,18 +30,44 @@ public class DemoServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String body = getBody(request);
+		System.out.println(body);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String body = getBody(request);
+		System.out.println(body);
 		
 	}
 	
+	private static String getBody(HttpServletRequest request) throws IOException{
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
 
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        }finally {
+            if(bufferedReader != null){
+                bufferedReader.close();
+
+                }
+            }
+        return stringBuilder.toString();
+
+
+    }
 
 }
