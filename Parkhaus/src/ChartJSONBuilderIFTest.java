0e8dd1d9cb525048e3bf2351194b5ba430d82c97
Felix Knorre <felix.knorre@smail.inf.h-brs.de>
2020-08-20 16:23:54 +0200

    converted into maven project

--- Parkhaus/src/ChartJSONBuilderIFTest.java
+++ Parkhaus/src/ChartJSONBuilderIFTest.java
@@ -1,34 +1,49 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ChartJSONBuilderIFTest {
 	
-	ChartJSONBuilderIF cjBar;
-	ChartJSONBuilderIF cjPie;
+	ParkhausIF ph;
+	AutoIF a1, a2, a3;
+	ChartJSONBuilderIF cjb;
+
 	
 	@BeforeEach
 	void setUp() {
-		String[] labels = {"Car_1", "Car_2", "Car_3"};
-		String[] values = {"20", "14", "23"};
-		cjBar = new ChartJSONBuilder(labels, values, "bar");
-		cjPie = new ChartJSONBuilder(labels, values, "pie");
+		ph = new Parkhaus();
 		
+		a1 = new Auto("1", "12234", "20", "200", "hbdfjsbdf", "#color1", "3", "any");
+		a2 = new Auto("2", "323432", "14", "300", "kjfdbvs", "#color2", "4", "any");
+		a3 = new Auto("3", "323432", "23", "300", "kjfdbvs", "#color2", "4", "Family");
+		
+		ph.addAuto(a1);
+		ph.addAuto(a2);
+		ph.addAuto(a3);
+		
+		ph.removeAuto(a1);
+		ph.removeAuto(a2);
+		ph.removeAuto(a3);
+	
+		cjb = new ChartJSONBuilder();		
 	}
 
 	@Test
-	void testBuildJSON() {
-		System.out.println(cjBar.buildJSON());
-		String expected = "{" + " \"data\": [" + " {" + " \"x\": [" + " \"Car_1\"," + " \"Car_2\"," + " \"Car_3\"" + " ]," + " \"y\": [" + " 20," + " 14," + " 23" + " ]," + " \"type\": \"bar\"" + " }" + " ]" + "}";
-		assertEquals(expected, cjBar.buildJSON());
+	@DisplayName("Build json for bar chart")
+	void testBuildJSON() throws IOException {
+		String expected = "{" + "\"data\":[" + "{" + "\"x\":[" + "\"Car_1\"," + "\"Car_2\"," + "\"Car_3\"" + "]," + "\"y\":[" + "\"20\"," + "\"14\"," + "\"23\"" + "]," + "\"type\":\"bar\"" + "}" + "]" + "}";
+		assertEquals(expected, cjb.buildJSON(ph, "bar"));
 	}
 	
 	@Test
-	void test_builder_jsonPie() {
-		System.out.println(cjPie.buildJSON());
-		String expected = "{\n" + " \"data\": [\n" + " {\n" + " \"labels\": [\n" + " \"Car_1\",\n" + " \"Car_2\",\n" + " \"Car_3\"\n" + " ],\n" + " \"values\": [\n" + " 20,\n" + " 14,\n" + " 23\n" + " ],\n" + " \"type\": \"bar\"\n" + " }\n" + " ]\n" + "}";
-		assertEquals(expected, cjPie.buildJSON());
+	@DisplayName("Build json for pie chart")
+	void test_builder_jsonPie() throws IOException {
+		String expected = "{" + "\"data\":[" + "{" + "\"labels\":[" + "\"any\"," + "\"Family\"" + "]," + "\"values\":[" + "2," + "1" + "]," + "\"type\":\"pie\"" + "}" + "]" + "}";
+		assertEquals(expected, cjb.buildJSON(ph, "pie"));
 	}
 
 }
